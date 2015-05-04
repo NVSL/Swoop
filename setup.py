3b0bd74c23ab069f1753d49bdae4a2c6ac4f407b
Steven Swanson <swanson@cs.ucsd.edu>
2015-05-04 21:50:54 +0000

    bug fix"
    git-svn-id: svn+ssh://bbfs-01.calit2.net/grw/Gordon/svn/trunk/Gadgets/Tools/CircuitsByCode@20837 2cf53259-273a-af40-bda6-69d31023b8ec

--- setup.py
+++ setup.py
@@ -13,16 +13,21 @@ class BuildSwoop(install):
         GenerateSwoop.main("Swoop.py")
         install.run(self)
 
-# in the setup function:
-
 
 setup(name='Swoop',
       version='0.2',
       description="Python Library for Working with CadSoft Eagle Files",
+      long_description="""Swoop is a library of Python objects for representing and manipulating
+Cadsoft Eagle board, schematic, and library files used in designing printed
+circuit boards (PCBs).  It parses an input Eagle file, creates a internal
+representation data structure that represents the file's contents,
+provides accessors and mutators to query, read, and modify those contents, and
+generates valid Eagle files as output.
+      """,
       author="NVSL, University of California San Diego",
       author_email="swanson@cs.ucsd.edu",
       url="http://nvsl.ucsd.edu/Swoop/",
-      py_modules=['Swoop', 'eagleDTD'],
+      py_modules=['Swoop'],
       requires=["lxml (>=3.4.2)", "jinja2 (>=2.7.3)", "Sphinx (>=1.3.1)"],
       scripts=["bin/checkEagle.py", "bin/fixEagle.py", "bin/cleanupEagle.py", "bin/mergeLibrary.py"],
       cmdclass={'install': BuildSwoop}

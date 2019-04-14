96dc4a43f5729ecf56d5a19ebd87fbf8aa1f9635
Steven Swanson <swanson@cs.ucsd.edu>
2019-04-14 00:22:45 -0700

    use latest lxml

--- setup.py
+++ setup.py
@@ -54,8 +54,8 @@ setup(name='Swoop',
           "Swoop" : ["Swoop.py.jinja", "eagle.dtd.diff", "*.dtd", "default.dru"]
       },
       #ext_modules = cythonize([Extension("*", ["Swoop/Swoop.pyx"], extra_compile_args=["-O4"])]),
-      install_requires=["lxml==3.6.2",  "Sphinx>=1.3.1","Jinja2>=2.7.3", "shapely>=1.5.13"],
-      setup_requires=["Jinja2>=2.7.3", "lxml>=3.4.2"],
+      install_requires=["lxml>=3.6.2",  "Sphinx>=1.3.1","Jinja2>=2.7.3", "shapely>=1.5.13"],
+      setup_requires=["Jinja2>=2.7.3", "lxml>=3.6.2"],
       include_package_data=True,
       entry_points={
         'console_scripts': [
